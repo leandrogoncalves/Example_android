b857646f97c5cd30d653e210e6946e6f2be45acc
Leandro <contato.leandrogoncalves@gmail.com>
2016-09-15 22:45:05 -0300

    Build version 1

---
+++ src/CalcularMedia/app/src/main/java/br/com/fiap/calcularmedia/MainActivity.java
@@ -0,0 +1,78 @@
+package br.com.fiap.calcularmedia;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editNome = (EditText) findViewById(R.id.txtNome);
+    EditText editP1 = (EditText) findViewById(R.id.txtP1);
+    EditText editP2 = (EditText) findViewById(R.id.txtP2);
+    EditText editSituacao = (EditText) findViewById(R.id.txtSituacao);
+    EditText editMedia = (EditText) findViewById(R.id.txtMedia);
+    Button botaoCalcular = (Button) findViewById(R.id.btnCalcular);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        botaoCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double p1 = Double.parseDouble(editP1.getText().toString());
+                double p2 = Double.parseDouble(editP2.getText().toString());
+                double media = (p1+p2) /2 ;
+                editMedia.setText(String.valueOf(media));
+                String situacao;
+                if (media >= 6){
+                    situacao = "Aprovado";
+                }else{
+                    situacao = "Reprovado";
+                }
+                editSituacao.setText(situacao);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
